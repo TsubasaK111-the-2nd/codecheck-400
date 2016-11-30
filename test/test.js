91444400e9a294594ec469721cfa371b1542580d
code-project <dev@code-check.io>
2016-11-30 05:54:00 +0000

    Initial commit

---
+++ test/test.js
@@ -0,0 +1,57 @@
+"use strict";
+
+let assert = require("chai").assert;
+let codecheck = require("codecheck");
+let app = codecheck.consoleApp(process.env.APP_COMMAND);
+
+describe("Technology specialist challenge 1-1", () => {
+  let testcases = require("./test_exam1-1.json");
+  testcases.forEach((testcase) => {
+    it(testcase.title, () => {
+      return app.codecheck("1", testcase.input)
+      .then(result => {
+        if (testcase.code == 0) {
+          assert.equal(result.code, testcase.code);
+          //assert.equal(result.stdout.join("\\n"), testcase.output.join("\\n"));
+        } else {
+          assert.equal((result.code >= testcase.code), true);
+          // Ignore output lines when status code is greater than 0.
+        }
+      });
+    });
+  });
+});
+describe("Technology specialist challenge 1-2", () => {
+  let testcases = require("./test_exam1-2.json");
+  testcases.forEach((testcase) => {
+    it(testcase.title, () => {
+      return app.codecheck("2", testcase.input)
+      .then(result => {
+        if (testcase.code == 0) {
+          assert.equal(result.code, testcase.code);
+          //assert.equal(result.stdout.join("\\n"), testcase.output.join("\\n"));
+        } else {
+          assert.equal((result.code >= testcase.code), true);
+          // Ignore output lines when status code is greater than 0.
+        }
+      });
+    });
+  });
+});
+describe("Technology specialist challenge 1-3", () => {
+  let testcases = require("./test_exam1-3.json");
+  testcases.forEach((testcase) => {
+    it(testcase.title, () => {
+      return app.codecheck("3", testcase.input)
+      .then(result => {
+        if (testcase.code == 0) {
+          assert.equal(result.code, testcase.code);
+          //assert.equal(result.stdout.join("\\n"), testcase.output.join("\\n"));
+        } else {
+          assert.equal((result.code >= testcase.code), true);
+          // Ignore output lines when status code is greater than 0.
+        }
+      });
+    });
+  });
+});
